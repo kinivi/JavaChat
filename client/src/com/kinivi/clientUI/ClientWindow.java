52685425f0fac7fb1137fd795b6a116894651f15
Nikita Kiselov <kinivi210999@gmail.com>
2017-09-02 19:14:43 +0300

    Initial commit2

--- client/src/com/kinivi/clientUI/ClientWindow.java
+++ client/src/com/kinivi/clientUI/ClientWindow.java
@@ -6,7 +6,7 @@ public class ClientWindow extends JFrame{
     private static final String ipAddr="10.10.10.10";
     private static final int port = 8189;
     private static final int WIDTH = 600;
-    private static final int HEIGHT =400;
+    private static final int HEIGHT =470;
     public static void main(String[] args) {
 
     }
